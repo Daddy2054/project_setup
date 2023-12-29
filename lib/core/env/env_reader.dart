6673ed012d87d5c28cb15106efa701cc37d9c458
Jean <acalto2@gmail.com>
2023-12-29 23:09:53 +0100

    8. Enviroment Configuration.mp4

---
+++ lib/core/env/env_reader.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import '../../flavors.dart';
+
+class EnvReader {
+  String getEnvFileName(Flavor flavor) {
+    switch (flavor) {
+      case Flavor.dev:
+        return ".dev.env";
+      case Flavor.qa:
+        return ".qa.env";
+      case Flavor.uat:
+        return ".uat.env";
+      case Flavor.prod:
+        return ".prod.env";
+
+      default:
+        throw Exception(".env file not found");
+    }
+  }
+
+  String getBaseUrl() {
+    return dotenv.get('BASE_URL');
+  }
+
+  String getApiKey() {
+    return dotenv.get('API_KEY');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
-FutureOr<void> main() async {
+void main() async {
+  await dotenv.load(fileName: ".dev.env");
   runApp(const App());
 }
