72a802dd3cd22ea2f881a5d6d428d7b8854d0c9f
Jean <acalto2@gmail.com>
2024-01-04 21:54:36 +0100

    3. Hive Database Encryption.mp4

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project_setup/common/logging/logging_provider.dart';
+import 'package:project_setup/core/db/hive_db.dart';
 
 import 'package:project_setup/core/env/env_reader.dart';
 import 'package:project_setup/core/flavor/flavor.dart';
@@ -9,12 +10,10 @@ import 'package:project_setup/main_widget.dart';
 
 import 'package:project_setup/core/providers/internet_connection_observer.dart';
 
-
 void mainApp(Flavor flavor) async {
-
 // An object that stores the state of the providers and allows overriding the behavior of a specific provider.
   final container = ProviderContainer();
-  
+
 // Read the env file with dotEnv
   final envReader = container.read(envReaderProvider);
   final envFile = envReader.getEnvFileName(flavor);
@@ -22,8 +21,11 @@ void mainApp(Flavor flavor) async {
 
   // Setup Logger
   container.read(setupLoggingProvider);
+
+  // setup the hive database
+  container.read(hiveDbProvider);
   
-    // Observer internet connection
+  // Observer internet connection
   container.read(internetConnectionObserverProvider);
 
   runApp(
