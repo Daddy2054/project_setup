55fa835d2321d5bdb6130f6de2b988f4be7a83cd
Jean <acalto2@gmail.com>
2024-01-13 22:49:23 +0100

    Android Splash Screen and App Icon.mp4

--- android/app/src/main/kotlin/com/example/project_setup/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/project_setup/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.example.project_setup
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import io.flutter.embedding.android.FlutterActivity
 
-  class MainActivity: FlutterFragmentActivity() {
+
+class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Aligns the Flutter view vertically with the window.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+          // Disable the Android splash screen fade out animation to avoid
+          // a flicker before the similar frame is drawn in Flutter.
+          splashScreen.setOnExitAnimationListener { 
+              splashScreenView -> splashScreenView.remove() 
+          }
+        }
+
+      super.onCreate(savedInstanceState)
+    }
+      
 }
