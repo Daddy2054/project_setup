a04b0e6a83ad5bfe66c86540a27fd0e6a92afbb5
Jean <acalto2@gmail.com>
2024-01-23 21:48:15 +0100

    SignUp Button Loading State.mp4

---
+++ lib/features/auth/signup/presentation/ui/widget/signup_button_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:project_setup/common/widget/button/primary_button.dart';
+import 'package:project_setup/features/auth/signup/presentation/controller/sign_up_controller.dart';
+
+class SignUpButtonWidget extends ConsumerWidget {
+  final VoidCallback onPressed;
+  const SignUpButtonWidget({Key? key, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading =
+        ref.watch(signUpControllerProvider.select((value) => value.isLoading));
+    return PrimaryButton(
+      text: 'SignUp',
+      isEnabled: true,
+      isLoading: isLoading,
+      onPressed: onPressed,
+    );
+  }
+}
+
